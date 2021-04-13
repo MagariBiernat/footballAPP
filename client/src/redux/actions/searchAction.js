fcf119e386afb6ec036f8e6cd1c9b3723e56cd93
Bartłomiej Biernat <123biernat123@gmail.com>
2021-04-13 16:45:37 +0200

    fix

--- client/src/redux/actions/searchAction.js
+++ client/src/redux/actions/searchAction.js
@@ -6,31 +6,11 @@ const API_KEY = process.env.REACT_APP_API_KEY
 export const searchAction = (searchValue) => (dispatch) => {
   dispatch({ type: LOADING_SEARCH })
 
-  fetch(`http://localhost:3010/api/football/${searchValue}`, {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error))
+  const data = {
+    prefixes: "player/search",
+    searchPrefix: "name",
+    searchValue: searchValue,
+  }
 
-  // axios(`/player/search?api_key=${API_KEY}&name=${searchValue}`, {
-  //   method: "GET",
-  //   mode: "no-cors",
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   withCredentials: true,
-  //   credentials: "same-origin",
-  // })
-  //   .then((response) => {
-  //     if (response.status === 200) {
-  //       console.log(response)
-  //       // dispatch({type: GET_SEARCH, payload: response.})
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.warn(err)
-  //   })
+  axios.post("/api/football", data).then((response) => console.log(response))
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,45 @@
 require("dotenv").config()
 
 const express = require("express")
+const bodyParser = require("body-parser")
 const request = require("request")
+const cors = require("cors")
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors({ credentials: true, origin: true }))
+app.options("*", cors())
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   next()
 })
 
+const API_KEY = process.env.API_KEY
+const basicURL = "http://api.isportsapi.com/sport/football/"
+
+app.post("/api/football", (req, res) => {
+  const { prefixes, searchPrefix, searchValue } = req.body
+
+  //validate
+
+  request(
+    {
+      url: `${basicURL}${prefixes}?api_key=${API_KEY}&${searchPrefix}=${searchValue}`,
+    },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: err.message })
+      }
+      res.json(JSON.parse(body))
+    }
+  )
+})
+
 app.get("/api/football/:searchValue", (req, res) => {
   const { searchValue } = req.params
-  const API_KEY = process.env.API_KEY
-  console.log(searchValue, API_KEY)
+
   request(
     {
       url: `http://api.isportsapi.com/sport/football/player/search?api_key=${API_KEY}&name=${searchValue}`,
@@ -21,7 +48,6 @@ app.get("/api/football/:searchValue", (req, res) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: "error", message: err.message })
       }
-
       res.json(JSON.parse(body))
     }
   )
