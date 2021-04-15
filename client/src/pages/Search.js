149293123ce18483aa0b4012e46f89bed5118437
Bartłomiej Biernat <123biernat123@gmail.com>
2021-04-15 15:23:49 +0200

    Update Search.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -27,17 +27,17 @@ const Search = () => {
         <div>
           <p>
             Footballers :
-            {searchResults?.footballers.length > 0
+            {searchResults?.footballers?.length > 0
               ? searchResults.footballers.length
               : 0}
           </p>
           <p>
             Teams :
-            {searchResults?.teams.length > 0 ? searchResults.teams.length : 0}
+            {searchResults?.teams?.length > 0 ? searchResults.teams.length : 0}
           </p>
           <p>
             Leagues :
-            {searchResults?.leagues.length > 0
+            {searchResults?.leagues?.length > 0
               ? searchResults.leagues.length
               : 0}
           </p>
@@ -45,19 +45,19 @@ const Search = () => {
         <div>
           <div>
             <h2>Footballers</h2>
-            {searchResults?.footballers.map((item, index) => (
+            {searchResults?.footballers?.map((item, index) => (
               <div>{item.name}</div>
             ))}
           </div>
           <div>
             <h2>Teams</h2>
-            {searchResults?.teams.map((item, index) => (
+            {searchResults?.teams?.map((item, index) => (
               <div>{item.name}</div>
             ))}
           </div>
           <div>
             <h2>Leagues</h2>
-            {searchResults?.leagues.map((item, index) => (
+            {searchResults?.leagues?.map((item, index) => (
               <div>{item.name}</div>
             ))}
           </div>
